d819a3b861e70ce7a82c949994bf252c2c5758c5
Keks <keks@htmlacademy.ru>
2023-02-21 15:03:07 +0300

    :heavy_check_mark: Сборка #3

---
+++ 3/js/functions.js
@@ -0,0 +1,47 @@
+//Функция проверки длины строки
+const isLessOrEqual = (string, length) => string.length <= length;
+
+isLessOrEqual('проверяемая строка', 20);
+
+//Палиндром
+const isPalindrome = (string) => {
+  const tempString = string
+    .toLowerCase()
+    .replaceAll(' ', '');
+  let reverseString = '';
+  for(let i = tempString.length - 1; i >= 0; i--) {
+    reverseString += tempString.at(i);
+  }
+  return tempString === reverseString;
+};
+
+isPalindrome('ДовОд');
+
+//Функция извлечения цифр из строк
+const extractNumb = (string) => {
+  if (typeof string === 'number') {
+    return string;
+  }
+  let result = '';
+  for (let i = 0; i < string.length; i++) {
+    if (!Number.isNaN(parseInt(string.at(i), 10))) {
+      result += string.at(i);
+    }
+  }
+  return parseInt(result, 10);
+};
+
+extractNumb('ECMAScript 2022');
+
+//Функция с дополненными указанными символами
+const myPadSt = (string, minLength, pad) => {
+  const actualPad = minLength - string.length;
+  if (actualPad <= 0) {
+    return string;
+  }
+  const tempPad = pad.slice(0, actualPad % pad.length);
+  const tempRepeat = pad.repeat(actualPad / pad.length);
+  return tempPad + tempRepeat + string;
+};
+
+myPadSt('qwerty', 4, '0');
